dccf3115178417920ef6b0c4c7eda7a20231ac98
Mateus Ferreira <mrf.mateus@hotmail.com>
2021-08-25 12:55:57 -0300

    Atribuição e ternário

--- js/script.js
+++ js/script.js
@@ -1,23 +1,14 @@
-// Crie uma array com os anos que o Brasil ganhou a copa
-// 1959, 1962, 1970, 1994, 2002
-const anosBrasilCampeaoCopaDoMundo = [1959, 1962, 1970, 1994, 2002];
+// Some 500 ao valor de scroll abaixo,
+// atribuindo o novo valor a scroll
+var scroll = 1000;
+scroll += 500;
+console.log(scroll);
 
-// Interaja com a array utilizando um loop, para mostrar
-// no console a seguinte mensagem, `O brasil ganhou a copa de ${ano}`
-anosBrasilCampeaoCopaDoMundo.forEach(function (ano) {
-  console.log(`O Brasil ganhou a copa de ${ano}`);
-});
-
-// Interaja com um loop nas frutas abaixo e pare ao chegar em Pera
-var frutas = ["Banana", "Maçã", "Pêra", "Uva", "Melancia"];
-for (i = 0; i < frutas.length; i++) {
-  console.log(frutas[i]);
-  if (frutas[i] === "Pêra") {
-    break;
-  }
-}
-
-// Coloque a última fruta da array acima em uma variável,
-// sem remover a mesma da array.
-const ultimaFruta = frutas[frutas.length - 1];
-console.log(ultimaFruta, frutas);
+// Atribua true para a variável darCredito,
+// caso o cliente possua carro e casa.
+// E false caso o contrário.
+var possuiCarro = true;
+var possuiCasa = true;
+var darCredito;
+darCredito = possuiCarro && possuiCasa;
+console.log(darCredito);
