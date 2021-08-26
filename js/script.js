f314abc179315a1983eea0d400840ef4362a692b
Mateus Ferreira <mrf.mateus@hotmail.com>
2021-08-25 21:53:25 -0300

    Escopo

--- js/script.js
+++ js/script.js
@@ -1,14 +1,34 @@
-// Some 500 ao valor de scroll abaixo,
-// atribuindo o novo valor a scroll
-var scroll = 1000;
-scroll += 500;
-console.log(scroll);
+// Por qual motivo o código abaixo retorna com erros?
+//Resposta: variáveis do tipo const e let não vazam o escopo do bloco, quem está fora do bloco não tem acesso a elas.
+// {
+//     var cor = 'preto';
+//     const marca = 'Fiat';
+//     let portas = 4;
+//   }
+//   console.log(cor, marca, portas);
+  
+  // Como corrigir o erro abaixo?  
+  
+const dois = 2;    
+function somarDois(x) {
+// const dois = 2;        
+return x + dois;
+}
 
-// Atribua true para a variável darCredito,
-// caso o cliente possua carro e casa.
-// E false caso o contrário.
-var possuiCarro = true;
-var possuiCasa = true;
-var darCredito;
-darCredito = possuiCarro && possuiCasa;
-console.log(darCredito);
+function dividirDois(x) {
+    return x / dois;
+}
+
+somarDois(4);
+dividirDois(6);
+
+// O que fazer para total retornar 500?
+const numero = 50;
+
+for(let numero = 0; numero < 10; numero++) {
+console.log(numero);
+}
+
+const total = 10 * numero;
+console.log(total);
+  
