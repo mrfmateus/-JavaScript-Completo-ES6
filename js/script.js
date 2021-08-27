744b62d626b7708e7464f8f4deffddf75acb7010
Mateus Ferreira <mrf.mateus@hotmail.com>
2021-08-26 21:48:08 -0300

    Manipulando DOM

--- js/script.js
+++ js/script.js
@@ -1,34 +1,13 @@
-// Por qual motivo o código abaixo retorna com erros?
-//Resposta: variáveis do tipo const e let não vazam o escopo do bloco, quem está fora do bloco não tem acesso a elas.
-// {
-//     var cor = 'preto';
-//     const marca = 'Fiat';
-//     let portas = 4;
-//   }
-//   console.log(cor, marca, portas);
-  
-  // Como corrigir o erro abaixo?  
-  
-const dois = 2;    
-function somarDois(x) {
-// const dois = 2;        
-return x + dois;
-}
+// Retorne o url da página atual utilizando o objeto window
+console.log(window.location.href);
 
-function dividirDois(x) {
-    return x / dois;
-}
+// Seleciona o primeiro elemento da página que
+// possua a classe ativo
+const elementoAtivo = document.querySelector('.ativo');
+console.log(elementoAtivo.innerText);
 
-somarDois(4);
-dividirDois(6);
+// Retorne a linguagem do navegador
+console.log(window.navigator.language);
 
-// O que fazer para total retornar 500?
-const numero = 50;
-
-for(let numero = 0; numero < 10; numero++) {
-console.log(numero);
-}
-
-const total = 10 * numero;
-console.log(total);
-  
+// Retorne a largura da janela 
+console.log(window.innerWidth);
